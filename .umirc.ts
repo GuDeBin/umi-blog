fcbedc219bfdff48ed952b98f1ac3766041aca34
GuDeBin <gudebin@outlook.com>
2022-10-21 22:35:05 +0800

    Site updated: 2022-10-21 22:35:05

--- .umirc.ts
+++ .umirc.ts
@@ -1,14 +1,5 @@
 export default {
   npmClient: "pnpm",
-  apiRoute: {
-    platform: "vercel",
-  },
-  routes: [
-    { path: "/", component: "index" },
-    { path: "/posts/create", component: "posts/create" },
-    { path: "/login", component: "login" },
-    { path: "/posts/:postId", component: "posts/post" },
-  ],
-  plugins: [require.resolve("@umijs/plugins/dist/tailwindcss")],
   tailwindcss: {},
+  plugins: ["@umijs/plugins/dist/tailwindcss"],
 };
--- README.md
+++
@@ -1,3 +0,0 @@
-# UmiJS
-
-This tutorial is from [开发一个 Blog](https://umijs.org/docs/tutorials/blog)
